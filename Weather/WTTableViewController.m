fcc7430b0a6e09b0f9a633b3435eb2fc029f0933
Scott Gardner <scott.gardner@mac.com>
2014-04-08 17:57:37 -0500

    Implement AFHTTPSessionManager

--- Weather/WTTableViewController.m
+++ Weather/WTTableViewController.m
@@ -16,7 +16,7 @@
 
 static NSString * const BaseURLString = @"http://www.raywenderlich.com/demos/weather_sample/";
 
-@interface WTTableViewController () <NSXMLParserDelegate>
+@interface WTTableViewController () <NSXMLParserDelegate, CLLocationManagerDelegate, UIActionSheetDelegate>
 @property(strong) NSDictionary *weather;
 
 // Needed only for XML parsing
@@ -104,9 +104,10 @@ static NSString * const BaseURLString = @"http://www.raywenderlich.com/demos/wea
     
     strongSelf.weather = (NSDictionary *)responseObject;
     strongSelf.title = @"PLIST Retrieved";
-    [self.tableView reloadData];
+    [strongSelf.tableView reloadData];
   } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-    [self showErrorRetrievingWeatherAlert:error];
+    __strong typeof(weakSelf)strongSelf = weakSelf;
+    [strongSelf showErrorRetrievingWeatherAlert:error];
   }];
   
   [operation start];
@@ -117,6 +118,8 @@ static NSString * const BaseURLString = @"http://www.raywenderlich.com/demos/wea
   AFHTTPRequestOperation *operation = [self operationForRequestWithFormat:@"xml"];
   operation.responseSerializer = [AFXMLParserResponseSerializer serializer];
   
+  __weak typeof(self)weakSelf = self;
+  
   [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
     NSXMLParser *XMLParser = (NSXMLParser *)responseObject;
     [XMLParser setShouldProcessNamespaces:YES];
@@ -124,7 +127,8 @@ static NSString * const BaseURLString = @"http://www.raywenderlich.com/demos/wea
     XMLParser.delegate = self;
     [XMLParser parse];
   } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-    [self showErrorRetrievingWeatherAlert:error];
+    __strong typeof(weakSelf)strongSelf = weakSelf;
+    [strongSelf showErrorRetrievingWeatherAlert:error];
   }];
   
   [operation start];
@@ -132,7 +136,8 @@ static NSString * const BaseURLString = @"http://www.raywenderlich.com/demos/wea
 
 - (IBAction)clientTapped:(id)sender
 {
-  
+  UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"AFHTTPSessionManager" delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:nil otherButtonTitles:@"HTTP GET", @"HTTP POST", nil];
+  [actionSheet showFromBarButtonItem:sender animated:YES];
 }
 
 - (IBAction)apiTapped:(id)sender
@@ -295,6 +300,49 @@ static NSString * const BaseURLString = @"http://www.raywenderlich.com/demos/wea
   self.weather = @{@"data" : self.xmlWeatherDict};
   self.title = @"XML Retrieved";
   [self.tableView reloadData];
+}
+
+#pragma mark - CLLocationManagerDelegate
+
+
+
+#pragma mark - UIActionSheetDelegate
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+  if (buttonIndex == [actionSheet cancelButtonIndex]) {
+    return;
+  }
+  
+  NSURL *baseUrl = [NSURL URLWithString:BaseURLString];
+  NSDictionary *parameters = @{@"format" : @"json"};
+  
+  AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc] initWithBaseURL:baseUrl];
+  manager.responseSerializer = [AFJSONResponseSerializer serializer];
+  
+  __weak typeof(self)weakSelf = self;
+  
+  if (buttonIndex == 0) { // GET
+    [manager GET:@"weather.php" parameters:parameters success:^(NSURLSessionDataTask *task, id responseObject) {
+      __strong typeof(weakSelf)strongSelf = weakSelf;
+      strongSelf.weather = responseObject;
+      strongSelf.title = @"HTTP GET";
+      [strongSelf.tableView reloadData];
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+      __strong typeof(weakSelf)strongSelf = weakSelf;
+      [strongSelf showErrorRetrievingWeatherAlert:error];
+    }];
+  } else if (buttonIndex == 1) { // POST
+    [manager POST:@"weather.php" parameters:parameters success:^(NSURLSessionDataTask *task, id responseObject) {
+      __strong typeof(weakSelf)strongSelf = weakSelf;
+      strongSelf.weather = responseObject;
+      strongSelf.title = @"HTTP POST";
+      [strongSelf.tableView reloadData];
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+      __strong typeof(weakSelf)strongSelf = weakSelf;
+      [strongSelf showErrorRetrievingWeatherAlert:error];
+    }];
+  }
 }
 
 @end
